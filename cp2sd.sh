c69b038e84dee2af6e6a47a38ef97966981ba9db
Chieh-jay.liu <b9515038@gmail.com>
2018-02-06 17:50:32 +0800

    [video_qt2] add a copy script to replace mannual work.
    Signed-off-by: Chieh-jay.liu <b9515038@gmail.com>

---
+++ cp2sd.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#
+# cp2sd.sh
+# Copyright (C) 2018 lucas <lucas@lucas>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+cp video_qt2/video_qt2 /media/lucas/6434-3634/
+ls -l /media/lucas/6434-3634/video_qt2
